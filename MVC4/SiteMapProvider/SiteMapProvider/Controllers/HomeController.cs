74e0df51d99f9c067ea292efb82fb8b4c4455692
Enrique Thedy <ethedy@gmail.com>
2015-11-11 04:39:39 -0300

    Agrego comentario

--- MVC4/SiteMapProvider/SiteMapProvider/Controllers/HomeController.cs
+++ MVC4/SiteMapProvider/SiteMapProvider/Controllers/HomeController.cs
@@ -4,6 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+/*
+ 
+  Para que funcione NO USAR el administrador de paquetes, sino la consola
+ 
+  EJECUTAR: 
+ 
+  Install-Package MvcSiteMapProvider -Version 3.3.6
+ 
+  Esta es la ultima version que no usa DI, por eso no arrancaba en el curso
+ 
+ */
 namespace SiteMapProvider.Controllers
 {
   public class HomeController : Controller
