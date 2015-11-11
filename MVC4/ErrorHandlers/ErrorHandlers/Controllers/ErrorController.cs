5a9915b57775d71fd7955246e718299be5d5d227
Enrique Thedy <ethedy@gmail.com>
2015-11-11 02:02:10 -0300

    Proyecto generado

---
+++ MVC4/ErrorHandlers/ErrorHandlers/Controllers/ErrorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ErrorHandlers.Controllers
+{
+  public class ErrorController : Controller
+  {
+    //  Lanza una excepcion que deberia ser capturada por el handler por defecto de ASP.NET
+    public ActionResult DefaultError()
+    {
+      throw new ApplicationException("DefaultError Action Error");
+      return new EmptyResult();
+    }
+
+    //  Lanza una excepcion que será capturada por el filtro de excepcion colocado en el mismo metodo
+    [HandleError(View = "HandlerAttributeError")]
+    public ActionResult HandlerAttributeError()
+    {
+      throw new ApplicationException("HandlerAttributeError Action Error");
+      return new EmptyResult();
+    }
+
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
---
+++ MVC4/ErrorHandlers/ErrorHandlers/Controllers/OnExceptionController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ErrorHandlers.Controllers
+{
+  public class OnExceptionController : Controller
+  {
+    //
+    // GET: /OnException/
+
+    public ActionResult GenerarError()
+    {
+      throw new ApplicationException("GenerarError OnException Action Error");
+      return new EmptyResult();
+    }
+
+    protected override void OnException(ExceptionContext filterContext)
+    {
+      if (filterContext.Exception is ApplicationException)
+      {
+        filterContext.Result = new ViewResult()
+        {
+          ViewName = "OnException",
+          ViewData = new ViewDataDictionary<string>(filterContext.Exception.Message)
+        };
+      }
+    }
+  }
+}
