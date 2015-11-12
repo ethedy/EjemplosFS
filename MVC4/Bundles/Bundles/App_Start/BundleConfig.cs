44dc76f0be47f5f0b8806d232d08a00adc371471
Enrique Thedy <ethedy@gmail.com>
2015-11-12 17:34:51 -0300

    Agrego ejemplo Bundles

---
+++ MVC4/Bundles/Bundles/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Web.Optimization;
+
+namespace Bundles
+{
+  public class BundleConfig
+  {
+    public static void RegisterBundles(BundleCollection bundles)
+    {
+      bundles.Add(new ScriptBundle("~/bundles/scripts")
+        .Include(
+          "~/Scripts/jquery-{version}.js", 
+          "~/Scripts/jquery.validate.js", 
+          "~/Scripts/jquery.validate.unobtrusive.js"));
+    }
+  }
+}
