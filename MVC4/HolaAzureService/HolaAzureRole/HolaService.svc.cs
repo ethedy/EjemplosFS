43fc2c5fc3e54169e103e935b52572bac1c2e34d
Enrique Thedy <ethedy@gmail.com>
2015-11-24 19:21:40 -0300

    Acomodar  solucion Azure

---
+++ MVC4/HolaAzureService/HolaAzureRole/HolaService.svc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace HolaAzureRole
+{
+  // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "HolaService" in code, svc and config file together.
+  // NOTE: In order to launch WCF Test Client for testing this service, please select HolaService.svc or HolaService.svc.cs at the Solution Explorer and start debugging.
+  public class HolaService : IHolaService
+  {
+    public string GetSaludo()
+    {
+      Trace.TraceInformation("Accediendo al servicio interno...");
+
+      //  como hago para obtener una propiedad desde la clase web role
+      Trace.TraceInformation("Funcionando...a punto de saludar...");
+      return "Hola Mundo Azure!!";
+    }
+  }
+}
