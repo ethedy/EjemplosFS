4592d99460d286e2ebe0ae8af8d0dc1383d00572
Enrique Thedy <ethedy@gmail.com>
2015-11-26 23:20:40 -0300

    bundles

---
+++ MVC4/Bundles/Bundles/Models/Modelo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bundles.Models
+{
+  public class Modelo
+  {
+    public string Propiedad { get; set; }
+
+    public int Propiedad1 { get; set; }
+  }
+}
