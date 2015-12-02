5805ed66151e78bd33ed6231120ecf0e904078f7
Enrique Thedy <ethedy@gmail.com>
2015-12-02 17:20:39 -0300

    Prueba Remota Funciona

--- MVC4/SimpleService/PruebaRemota/Program.cs
+++ MVC4/SimpleService/PruebaRemota/Program.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using PruebaRemota.AzureServiceRemoto;
+
 namespace PruebaRemota
 {
   class Program
   {
     static void Main(string[] args)
     {
+      ISimpleService srvc = new SimpleServiceClient();
+
+      Console.WriteLine(srvc.GetData());
+      Console.ReadLine();
     }
   }
 }
